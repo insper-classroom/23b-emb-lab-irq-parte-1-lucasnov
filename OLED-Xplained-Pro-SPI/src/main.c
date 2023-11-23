93a326732c184a55fcb3df326da95e4fc020adf8
Lucas Novais <92753945+lucasnov@users.noreply.github.com>
2023-11-23 13:30:04 -0300

    correcao

--- OLED-Xplained-Pro-SPI/src/main.c
+++ OLED-Xplained-Pro-SPI/src/main.c
@@ -1,45 +1,149 @@
-#include <asf.h>
+/************************************************************************
+ * 5 semestre - Eng. da Computao - Insper
+ * Rafael Corsi - rafael.corsi@insper.edu.br
+ *
+ * Material:
+ *  - Kit: ATMEL SAME70-XPLD - ARM CORTEX M7
+ *
+ * Objetivo:
+ *  - Demonstrar interrupção do PIO
+ *
+ * Periféricos:
+ *  - PIO
+ *  - PMC
+ *
+ * Log:
+ *  - 10/2018: Criação
+ ************************************************************************/
 
-#include "gfx_mono_ug_2832hsweg04.h"
-#include "gfx_mono_text.h"
-#include "sysfont.h"
+/************************************************************************/
+/* includes                                                             */
+/************************************************************************/
 
-int main (void)
+#include "asf.h"
+
+/************************************************************************/
+/* defines                                                              */
+/************************************************************************/
+
+// LED
+#define LED_PIO      PIOC
+#define LED_PIO_ID   ID_PIOC
+#define LED_IDX      8
+#define LED_IDX_MASK (1 << LED_IDX)
+
+// Botão
+#define BUT_PIO      PIOA
+#define BUT_PIO_ID   ID_PIOA
+#define BUT_IDX  11
+#define BUT_IDX_MASK (1 << BUT_IDX)
+
+/************************************************************************/
+/* constants                                                            */
+/************************************************************************/
+
+/************************************************************************/
+/* variaveis globais                                                    */
+/************************************************************************/
+
+/************************************************************************/
+/* prototype                                                            */
+/************************************************************************/
+void io_init(void);
+void pisca_led(int n, int t);
+
+/************************************************************************/
+/* handler / callbacks                                                  */
+/************************************************************************/
+
+/*
+ * Exemplo de callback para o botao, sempre que acontecer
+ * ira piscar o led por 5 vezes
+ *
+ * !! Isso é um exemplo ruim, nao deve ser feito na pratica, !!
+ * !! pois nao se deve usar delays dentro de interrupcoes    !!
+ */
+
+volatile char flag_botao;
+void but_callback(void)
 {
-	board_init();
+  flag_botao = 1;
+}
+
+/************************************************************************/
+/* funções                                                              */
+/************************************************************************/
+
+// pisca led N vez no periodo T
+void pisca_led(int n, int t){
+  for (int i=0;i<n;i++){
+    pio_clear(LED_PIO, LED_IDX_MASK);
+    delay_ms(t);
+    pio_set(LED_PIO, LED_IDX_MASK);
+    delay_ms(t);
+  }
+}
+
+// Inicializa botao SW0 do kit com interrupcao
+void io_init(void)
+{
+
+  // Configura led
+	pmc_enable_periph_clk(LED_PIO_ID);
+	pio_configure(LED_PIO, PIO_OUTPUT_0, LED_IDX_MASK, PIO_DEFAULT);
+
+  // Inicializa clock do periférico PIO responsavel pelo botao
+	pmc_enable_periph_clk(BUT_PIO_ID);
+
+  // Configura PIO para lidar com o pino do botão como entrada
+  // com pull-up
+	pio_configure(BUT_PIO, PIO_INPUT, BUT_IDX_MASK, PIO_PULLUP);
+
+  // Configura interrupção no pino referente ao botao e associa
+  // função de callback caso uma interrupção for gerada
+  // a função de callback é a: but_callback()
+  pio_handler_set(BUT_PIO,
+                  BUT_PIO_ID,
+                  BUT_IDX_MASK,
+                  PIO_IT_FALL_EDGE,
+                  but_callback);
+
+  // Ativa interrupção e limpa primeira IRQ gerada na ativacao
+  pio_enable_interrupt(BUT_PIO, BUT_IDX_MASK);
+  pio_get_interrupt_status(BUT_PIO);
+  
+  // Configura NVIC para receber interrupcoes do PIO do botao
+  // com prioridade 4 (quanto mais próximo de 0 maior)
+  NVIC_EnableIRQ(BUT_PIO_ID);
+  NVIC_SetPriority(BUT_PIO_ID, 4); // Prioridade 4
+}
+
+/************************************************************************/
+/* Main                                                                 */
+/************************************************************************/
+
+// Funcao principal chamada na inicalizacao do uC.
+void main(void)
+{
+	
+	// Inicializa clock
 	sysclk_init();
-	delay_init();
 
-  // Init OLED
-	gfx_mono_ssd1306_init();
-  
-  
-	gfx_mono_draw_filled_circle(20, 16, 16, GFX_PIXEL_SET, GFX_WHOLE);
-  gfx_mono_draw_string("mundo", 50,16, &sysfont);
-  
-  
+	// Desativa watchdog
+	WDT->WDT_MR = WDT_MR_WDDIS;
 
-  /* Insert application code here, after the board has been initialized. */
-	while(1) {
+	  // configura botao com interrupcao
+	  io_init();
+	  int delay = 300;
 
-
-
-			// Escreve na tela um circulo e um texto
-			
-			for(int i=70;i<=120;i+=2){
-				
-				gfx_mono_draw_rect(i, 5, 2, 10, GFX_PIXEL_SET);
-				delay_ms(10);
-				
-			}
-			
-			for(int i=120;i>=70;i-=2){
-				
-				gfx_mono_draw_rect(i, 5, 2, 10, GFX_PIXEL_CLR);
-				delay_ms(10);
-				
-			}
-			
-			
+	// super loop
+	// aplicacoes embarcadas no devem sair do while(1).
+	while(1)
+  {
+	if(flag_botao){
+		pisca_led(5,delay);
+		flag_botao=0;
+	  }
+	  pmc_sleep(SAM_PM_SMODE_SLEEP_WFI);
 	}
 }
